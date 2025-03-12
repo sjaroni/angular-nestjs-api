73e8f99a62d3e5acb832cee799d674d0ef72dfdb
Stefan Jaroni <sjaroni@outlook.de>
2025-03-12 21:47:38 +0100

    refactoring: add dataservice, catchErrors with rxjs

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs';
 })
 export class DataService {
   private http = inject(HttpClient);
-  // private apiUrl = 'https://jsonplaceholder.typicode.com/posts'; // Beispiel-API
-  private apiUrl = 'http://localhost:3000/movis';
+  //private apiUrl = 'https://jsonplaceholder.typicode.com/posts'; // Beispiel-API
+  private apiUrl = 'http://localhost:3000/movies';
 
   getData(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
